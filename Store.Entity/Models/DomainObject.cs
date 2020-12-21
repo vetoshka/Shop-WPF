1cf22ce6fcaffa4e0465bb9925c1add531b0cb8d
vetoshka <anyakolomiec00@gmail.com>
2020-12-21 23:25:00 +0200

    Create GenericDataService

---
+++ Store.Entity/Models/DomainObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Domain.Models
+{
+  public class DomainObject
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Store.EntityFramework/Services/GenericDataService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Store.Domain.Models;
+using Store.Domain.Services;
+using Store.EntityFramework;
+
+namespace Store.Domain.Data
+{
+    public class GenericDataService<T> : IDataService<T> where T : DomainObject
+    {
+        private readonly StoreDbContextFactory _storeDbContextFactory;
+        public async Task<bool> Delete(Guid id)
+        {
+            using (StoreDbContext context = _storeDbContextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+                context.Set<T>().Remove(entity);
+                await context.SaveChangesAsync();
+                return true;
+            }
+        }
+
+        public async Task<T> Get(Guid id)
+        {
+            using (StoreDbContext context = _storeDbContextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+                return entity;
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            using (StoreDbContext context = _storeDbContextFactory.CreateDbContext())
+            {
+                 return await context.Set<T>().ToListAsync();
+            }
+        }
+
+        public async Task<T> Insert(T entity)
+        {
+            using (StoreDbContext context = _storeDbContextFactory.CreateDbContext())
+            {
+               var createdEntity= await context.Set<T>().AddAsync(entity);
+                await context.SaveChangesAsync();
+                return createdEntity.Entity;
+            }
+        }
+
+        public async Task<T> Update(Guid id, T entity)
+        {
+            using (StoreDbContext context = _storeDbContextFactory.CreateDbContext())
+            {
+                entity.Id = id;
+                context.Set<T>().Update(entity);
+                await context.SaveChangesAsync();
+                return entity;
+            }
+        }
+    }
+}
